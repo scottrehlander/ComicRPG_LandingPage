3be38d164a1fbef0b96002b6af4e08366d29fe7c
scoot <scott@rehlander.com>
2017-02-10 20:21:10 -0500

    initial commit

---
+++ Scripts/ComicRpgScripts.js
@@ -0,0 +1,8 @@
+﻿function swapImages() {
+    var $active = $('#myGallery .active');
+    var $next = ($('#myGallery .active').next().length > 0) ? $('#myGallery .active').next() : $('#myGallery img:first');
+    $active.fadeOut(function () {
+        $active.removeClass('active');
+        $next.fadeIn().addClass('active');
+    });
+}
